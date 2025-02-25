12d6c13a4db46e4827ddaec2a0e9e3875e4ca0b3
Vinicius França <viniciusdasilva10@hotmail.com>
2025-02-24 21:12:20 -0300

    Initial Commit

---
+++ CoinApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinApp
+//
+//  Created by Vinicius França on 24/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
